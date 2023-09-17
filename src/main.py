aeeb11f7c333dd4466fad5964894918649c40fd8
Matthew Bastow <matthewbastow93@gmail.com>
2023-09-17 22:43:55 +0100

    Strict mypy check passing but tests and linter now failing instead.

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ import json
 from json import JSONDecodeError
 import os
 from pathlib import Path
+from typing import Any, TypedDict, List, Optional
 
 # custom error classes
 from result import Result, Ok, Err, as_result
@@ -26,6 +27,16 @@ from pydeequ.checks import Check, CheckLevel # noqa: E402
 from pydeequ.verification import VerificationSuite, VerificationResult # noqa: E402
 
 
+CheckResult = TypedDict('CheckResult', {
+	'check_status': str,
+	'check_level': str,
+	'constraint_status': str,
+	'check': str,
+	'constraint_message': str,
+	'constraint': str
+})
+
+
 class ConstraintError(Exception):
     """A custom error used to report that a dataframe has failed a constraint 
     check.
@@ -34,13 +45,13 @@ class ConstraintError(Exception):
     message -- developer defind message for the error
     """
 
-    def __init__(self, constraint: str, status: str):
-        self.constraint = constraint
-        self.status = status
+    def __init__(self, constraint: Optional[str], status: Optional[str]):
+        self.constraint = str(constraint)
+        self.status = str(status)
 
         message = \
-            "Constraint " + constraint + \
-            " returned status " + status + \
+            "Constraint " + self.constraint + \
+            " returned status " + self.status + \
             "."
 
         super().__init__(message)
@@ -52,30 +63,30 @@ def main() -> None:
 	match start_spark():
 		case Ok(session):
 			print("Spark started.")
-		case Err(e):
-			print(e)
+		case Err(start_e):
+			print(start_e)
 			return
 
 	match extract(session, Path("./src/schema.json"), Path("./data/titanic.csv")):
 		case Ok(df):
 			print("Data extracted.")
-		case Err(e):
-			print(e)
+		case Err(extract_e):
+			print(extract_e)
 			return
 
 	match validate(session, df):
 		case Ok(None):
 			print("Dataframe validated.")
-		case Err(e):
-			print(e)
+		case Err(val_e):
+			print(val_e)
 			return
 
 
 	match load(df, Path("./data/valid_titanic.csv")):
 		case Ok(None):
 			print("Data written to destination.")
-		case Err(e):
-			print(e)
+		case Err(load_e):
+			print(load_e)
 			return
 
 	shutdown_spark(session)
@@ -99,12 +110,9 @@ def start_spark() -> SparkSession:
 	return session
 
 
-@as_result(OSError, JSONDecodeError, PySparkException)
-def extract(session: SparkSession, schema_path: Path, data_path: Path) -> DataFrame:
+def extract(session: SparkSession, schema_path: Path, data_path: Path) -> Result[DataFrame, PySparkException]:
 	"""Read the specified csv file into a Spark dataframe. Returns the dataframe.
 
-	Returns OSError if the schema file fails to open.
-	Returns JSONDecodeError if the schema file can't be parsed to json.
 	Returns PySparkException if Spark fails to create a Dataframe
 
 	Keyword arguments:
@@ -113,26 +121,75 @@ def extract(session: SparkSession, schema_path: Path, data_path: Path) -> DataFr
 	data_path -- a path that points at the csv file of data
 	"""
 
+	infer_schema = False
+
 	# load the datasets schema
-	with open(schema_path) as f:
-		raw_schema = json.load(f)
-	
-	schema = StructType.fromJson(raw_schema)
+	match load_schema(schema_path):
+		case Ok(raw_schema):
+			schema = StructType.fromJson(raw_schema)
+		case Err(e):
+			infer_schema = True
+			schema = StructType([])
 
 	# load a csv file checking the schema as the data is loaded
 	df = session \
 		.read \
 		.format("csv") \
-		.option("header","true") \
-		.schema(schema) \
-		.load(str(data_path))
+		.option("header","true")
 
-	return df
+	if infer_schema:
+		df = df.schema(schema)
+	else:
+		df = df.option("inferSchema", infer_schema)
+
+	return Ok(df.load(str(data_path)))
 
 
-@as_result(Exception)
-def validate(session: SparkSession, df: DataFrame) -> None:
+@as_result(OSError, JSONDecodeError, TypeError)
+def load_schema(schema_path: Path) -> dict[str, Any]:
+	"""Read the specified json file and decode into a python dict.
+
+	Returns OSError if the schema file fails to open.
+	Returns JSONDecodeError if the schema file can't be parsed to json.
+
+	Keyword arguments:
+	schema_path -- a path that points at the tables schema
+	"""
+
+	with open(schema_path) as f:
+		raw_schema = json.load(f)
+
+	match raw_schema:
+		case dict():
+			return raw_schema
+		case _:
+			raise TypeError
+
+
+def validate(session: SparkSession, df: DataFrame) -> Result[None, ConstraintError]:
 	"""Add data quality constraints to the specified dataframe.
+	
+	Using pydeequ to perform the checks: https://github.com/awslabs/python-deequ
+
+    Keyword arguments:
+    session -- the apark session used to create a dataframe
+    df -- a spark dataframe whose data needs validating
+	"""
+
+	match check_constraints(session, df):
+		
+		case Ok(check_results):
+			return parse_check_results(check_results)
+		
+		case Err(e):
+			return Err(ConstraintError(constraint=e.getErrorClass(), status="Failure"))
+
+	return Ok(None)
+
+
+@as_result(PySparkException)
+def check_constraints(session: SparkSession, df: DataFrame) -> List[CheckResult]:
+	"""Add and check data quality constraints on the specified dataframe.
 	
 	Using pydeequ to perform the checks: https://github.com/awslabs/python-deequ
 
@@ -155,15 +212,28 @@ def validate(session: SparkSession, df: DataFrame) -> None:
 		).run()
 
 	# parse the check results as a list of dicts
-	check_result_json = VerificationResult.checkResultsAsJson(session, check_result)
+	check_result_json: List[CheckResult] = VerificationResult.checkResultsAsJson(session, check_result)
 
-	# check if any of the constraints failed
-	for constraint in check_result_json:
+	return check_result_json
+
+
+def parse_check_results(check_results: List[CheckResult]) -> Result[None, ConstraintError]:
+	"""Check if any of the constraints failed.
+
+	Raise a ConstraintError if a check failed.
+
+    Keyword arguments:
+    session -- the apark session used to create a dataframe
+    df -- a spark dataframe whose data needs validating
+	"""
+	for constraint in check_results:
 		if constraint.get("constraint_status") != "Success":
-			raise ConstraintError(
+			return Err(ConstraintError(
 				constraint=constraint.get("constraint"), 
 				status=constraint.get("constraint_status")
-			)
+			))
+
+	return Ok(None)
 
 
 def load(df: DataFrame, filepath: Path) -> Result[None, PySparkException]:
